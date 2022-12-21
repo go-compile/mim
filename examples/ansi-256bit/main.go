5bcbda8668984e79f61f5dc20f71b9a2ed6a1f36
go-compile <97609133+go-compile@users.noreply.github.com>
2022-12-21 17:48:26 +0000

    feat: ANSI256 tile 4x4 based in input size & add ANSI TrueColour RGB Mozaic

--- examples/ansi-256bit/main.go
+++ examples/ansi-256bit/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
 
 	"github.com/go-compile/mim"
@@ -9,6 +10,16 @@ import (
 
 func main() {
 	fingerprint := sha256.Sum256([]byte("certificate contents would typically go here"))
+
+	fmt.Println(mim.New(fingerprint[:]).ANSI256())
+
+	fmt.Println()
+
+	sha512Mozaic()
+}
+
+func sha512Mozaic() {
+	fingerprint := sha512.Sum512([]byte("certificate contents would typically go here"))
 
 	fmt.Println(mim.New(fingerprint[:]).ANSI256())
 }
--- mozaic.go
+++ mozaic.go
@@ -12,15 +12,43 @@ func New(fingerprint []byte) Mozaic {
 }
 
 func (m Mozaic) ANSI256() (output string) {
-	for i := 0; i < len(m); i++ {
-		if i%8 == 0 && i != 0 {
+	lm := len(m)
+	// rowWidth := lm / 8
+	rows := lm / 4
+
+	for i := 0; i < lm; i++ {
+		if i%rows == 0 && i != 0 {
 			output += "\r\n"
 		} else if i%4 == 3 && i != 0 { // per row, every fourth byte add space and create parallel square
 			output += colourANSI256(m[i]) + "  \x1b[0m  "
 			continue
 		}
 
-		output += colourANSI256(m[i]) + "  "
+		output += colourANSI256(m[i]) + "  \x1b[0m"
+	}
+
+	return output + "\x1b[0m"
+}
+
+func (m Mozaic) ANSITrueColour() (output string) {
+	// pre-computed length of m
+	lm := len(m)
+
+mainloop:
+	for i := 0; i < lm; i += 3 {
+
+		// if there are less than 3 bytes left
+		if x := lm - i; x < 3 {
+			switch x {
+			case 1:
+				output += colourANSITrueColour(m[i], 0, 0) + "  "
+			default:
+				output += colourANSITrueColour(m[i], m[i+1], 0) + "  "
+				break mainloop
+			}
+		}
+
+		output += colourANSITrueColour(m[i], m[i+1], m[i+2]) + "  "
 	}
 
 	return output + "\x1b[0m"
@@ -29,4 +57,9 @@ func (m Mozaic) ANSI256() (output string) {
 // colourANSI256 takes a byte and returns a 256bit ANSI colour code
 func colourANSI256(d byte) string {
 	return fmt.Sprintf("\x1b[48;5;%dm", d)
+}
+
+// colourANSITrueColour takes three bytes and returns a RGB ANSI colour code
+func colourANSITrueColour(r, g, b byte) string {
+	return fmt.Sprintf("\x1b[38;2;%d;%d;%d;48;2;%d;%d;%dm", r, g, b, r, g, b)
 }
