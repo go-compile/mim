b0bd028e8a1a5d98eee696672e32f844c58da599
go-compile <97609133+go-compile@users.noreply.github.com>
2022-12-28 11:09:03 +0000

    feat: add SVG and custom render support

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/go-compile/mim
 
 go 1.18
 
-require golang.org/x/crypto v0.4.0
+require (
+	github.com/ajstarks/gensvg v0.0.0-20210923152200-4042c242e95e
+	golang.org/x/crypto v0.4.0
+)
---
+++ render.go
@@ -0,0 +1,70 @@
+package mim
+
+import (
+	"encoding/hex"
+	"io"
+
+	"github.com/ajstarks/gensvg"
+)
+
+const (
+	// SquareSize is used to render the mozaic
+	SquareSize = float64(20)
+
+	// CanvasWidth is used to create a SVG or PNG canvas for the mozaic
+	CanvasWidth = 380
+	// CanvasHeight is used to create a SVG or PNG canvas for the mozaic
+	CanvasHeight = 80
+)
+
+// SVG will produce a SVG mozaic the size of 380 by 80px
+func (m Mozaic) SVG(w io.Writer) {
+	s := gensvg.New(w)
+	s.Start(CanvasHeight, CanvasHeight)
+
+	m.Render(func(x, y float64, colour [3]byte) {
+		s.Square(x, y, 20, "fill: "+rgbToHEX(colour))
+	})
+
+	s.End()
+}
+
+// Render allows you to provide a custom plotting function which
+// can then be used to produce your own image using any render engine.
+func (m Mozaic) Render(plot func(x, y float64, colour [3]byte)) {
+	lm := len(m)
+	// rowWidth := lm / 8
+	rows := lm / 4
+
+	yOffset := float64(0)
+	xOffset := float64(0)
+
+	for i := 0; i < lm; i++ {
+		if i%rows == 0 && i != 0 {
+			// create new row
+			yOffset += SquareSize
+			xOffset = 0
+		} else if i%2 == 1 && i != 0 { // per row, every fourth byte add space and create parallel square
+			l, r := splitUint8(m[i])
+
+			plot(xOffset, yOffset, colour[l])
+			xOffset += SquareSize
+			plot(xOffset+SquareSize, yOffset, colour[r])
+
+			xOffset += SquareSize * 3
+
+			continue
+
+		}
+
+		l, r := splitUint8(m[i])
+
+		plot(xOffset, yOffset, colour[l])
+		xOffset += SquareSize
+		plot(xOffset+SquareSize, yOffset, colour[r])
+	}
+}
+
+func rgbToHEX(rgb [3]byte) string {
+	return "#" + hex.EncodeToString(rgb[:])
+}
